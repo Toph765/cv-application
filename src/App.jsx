4ba8d2970439eca693da335f59c0384ccf2bfbce
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-12-14 15:06:43 +0800

    App: import PracExp

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import GenInfo from './GenInfo'
 import EdExp from './EdExp'
+import PracExp from './PracExp'
 
 function App() {
   return (
     <>
       <GenInfo />
       <EdExp />
+      <PracExp />
     </>
   )
 }
