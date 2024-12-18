c0ca46a1aac5e6fa3cd04e80a1d7ed0d8ecd40b4
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-12-18 14:45:12 +0800

    PracExp: add responsibilities dynamically

--- src/PracExp.jsx
+++ src/PracExp.jsx
@@ -8,10 +8,21 @@ function PracExp({callback}) {
     from: '',
     to: '',
   })
+  
+  const [resp, setResp] = useState({
+    id: 0,
+    resp: '',
+  });
 
   const grabData = (e) => {
     e.preventDefault();
     callback(pracExp);
+  }
+
+  const addResp = (e) => {
+    e.preventDefault();
+    pracExp.responsibilities.push(resp);
+    setResp({ ...resp, id: resp.id + 1 });
   }
 
     return (
@@ -26,15 +37,18 @@ function PracExp({callback}) {
               <input id="position" type="text" onChange={(e) => setPracExp({...pracExp, position: e.target.value})} />      
             </div>
             <div>
+              <fieldset>
               <label htmlFor="responsibilities">Responsibilities:</label>        
-              <input id="responsibilities" type="text" />      
+              <input id="responsibilities" type="text" onChange={(e) => setResp({...resp, resp: e.target.value })} />      
+                <button onClick={addResp}>add</button>
+              </fieldset>
             </div>
             <div>
               <input type="date" onChange={(e) => setPracExp({...pracExp, from: e.target.value})} />     
               <input type="date" onChange={(e) => setPracExp({...pracExp, to: e.target.value})}/>      
             </div>
             <button>edit</button>    
-            <button onClick={grabData}>submit</button>    
+            <button onClick={grabData} type="submit">submit</button>    
           </form>      
       </>
   )
