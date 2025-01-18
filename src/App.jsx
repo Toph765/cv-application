25c46e39ade7888eca187b08ce51dc93c8dce5b3
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-01-18 17:20:20 +0800

    App: grabSkills func

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import PracExp from './PracExp'
 import Skill from './Skill'
 
 function App() {
-  let genInfo, edExp, pracExp;
+  let genInfo, edExp, pracExp, skills;
 
   const grabGenInfo = (data) => {
     console.log(data);
@@ -23,12 +23,17 @@ function App() {
     return pracExp = data;
   }
 
+  const grabSkills = (data) => {
+    console.log(data);
+    return skills = data;
+  }
+
   return (
     <>
       <GenInfo callback={grabGenInfo} />
       <EdExp callback={grabEdExp} />
       <PracExp callback={grabPracExp} />
-      <Skill/>
+      <Skill callback={grabSkills} />
     </>
   )
 }
