63bb912139c4a050f8c8b4802a472a0ba347f6ce
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-01-18 17:00:23 +0800

    Skill: handleInputChange func

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -6,21 +6,36 @@ function Skill() {
     skill: '',
   }]);
     
+  const handleInputChange = (e, id) => {
+    const { name, value } = e.target; 
+    const list = [...skills];
+    list.filter(item => item.id === id)[0][name] = value;
+    setSkills(list);  
+  }
+    
   return (
     <>
       <form>
         <div>
           <label>
+            Skills: 
             {skills.map((item) => {
               return (
                 <div key={item.id}>
-                  <input name="skill" type="text" onChange={(e) => handleInputChange(e, item.id)} />
+                  <input
+                      value={item.skill}    
+                      name="skill"
+                      type="text"
+                      onChange={(e) => handleInputChange(e, item.id)} />
                   {skills.length > 1 && (<button onClick={(e) => handleRemoveBtn(e, item.id)}>X</button>)}
                 </div>
               )})}
           </label>
-          <button onClick={handleAddBtn}>add</button>        
+          <button>add</button>
+          <button>Submit</button>        
         </div>
       </form>
     </>
   )}
+
+export default Skill;
