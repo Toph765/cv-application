01774ddb6a356c8f54bbf17e11ad20e1fc0907f4
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-02-01 17:10:09 +0800

    *: change  tag to section; add tags

--- src/App.jsx
+++ src/App.jsx
@@ -39,25 +39,30 @@ function App() {
 
   return (
     <>
-    <main>
-      <div>
+      <main>
+      <section className='section-container'>
+        <h1>CV-Maker</h1>
         <GenInfo callback={grabGenInfo} isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}/>
         <EdExp callback={grabEdExp}  isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}/>
         <PracExp callback={grabPracExp}  isActive={activeIndex === 2} onShow={() => setActiveIndex(2)}/>
         <Skill callback={grabSkills}  isActive={activeIndex === 3} onShow={() => setActiveIndex(3)}/>
-      </div>
+      </section>
       <div className='cv-container'>
           <ShowGenInfo obj={genInfo} />
+          <hr />
           <div>
             <div>Education</div>
+            <hr />
             <ShowEdExp data={edExp} />
           </div>
           <div>
             <div>Experience</div>
+            <hr />
             <ShowPracExp data={pracExp} />
           </div>
           <div>
             <div>Skills</div>
+            <hr />
             <ShowSkills data={skills}/>
           </div>
       </div>  
